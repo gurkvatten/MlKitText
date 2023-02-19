e12f19856f3422056dfe18086754ee8833fbfe31
Johan Karlsson <93186588+gurkvatten@users.noreply.github.com>
2023-02-19 14:05:36 +0100

    Added new class for custom camera

---
+++ app/src/main/java/com/example/mlkittext/CameraClass.kt
@@ -0,0 +1,11 @@
+package com.example.mlkittext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraClass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_class)
+    }
+}
